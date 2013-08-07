42cbf8ed5618ce28eb9c75419f4bb0cbbd29761f
Laura Erskine <lrskne@gmail.com>
2013-08-06 17:18:36 -0700

    Empty Rails 4.0 app, only a rails new at this point

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogWhiz::Application.config.secret_key_base = '854ca0a67d7926827706b494435789702ae74af134d2ccb8c45623a920932db90404c86e1f3094096426950727f3de6c0898290b066a9a2ba82f2fcc180507f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogWhiz::Application.config.session_store :cookie_store, key: '_BlogWhiz_session'
